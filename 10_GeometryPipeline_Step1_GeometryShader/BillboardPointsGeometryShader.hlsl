7519e8b9c496bd7b4f9269737d3fd9f7baa1513e
unknown <msjjang0315@naver.com>
2025-08-15 11:36:38 +0900

    Geometry Shader

--- 10_GeometryPipeline_Step1_GeometryShader/BillboardPointsGeometryShader.hlsl
+++ 10_GeometryPipeline_Step1_GeometryShader/BillboardPointsGeometryShader.hlsl
@@ -25,29 +25,45 @@ struct PixelShaderInput
 };
 
 //TODO: PointStream -> TriangleStream
-[maxvertexcount(100)] // 최대 출력 Vertex 갯수
+[maxvertexcount(4)] // 최대 출력 Vertex 갯수
 void main(point GeometryShaderInput input[1], uint primID : SV_PrimitiveID,
-                              inout PointStream<PixelShaderInput> outputStream)
+                              inout TriangleStream<PixelShaderInput> outputStream)
 {
     
-    // float hw = 0.5 * width;
+    float hw = 0.5 * width;
     
     PixelShaderInput output;
     
-    output.pos = input[0].pos;
-    
-    for (int i = 0; i < 100; i ++)
-    {
-        output.pos = input[0].pos + float4(0.0, 0.003, 0.0, 0.0) * float(i);
-        output.pos = mul(output.pos, view);
-        output.pos = mul(output.pos, proj);
-        output.primID = primID;
+    output.pos = input[0].pos + float4(-hw, -hw, 0, 0);
+    output.pos = mul(output.pos, view);
+    output.pos = mul(output.pos, proj);
+    output.primID = primID;
 
-        outputStream.Append(output);
-    }
+    outputStream.Append(output);
+    
+    output.pos = input[0].pos + float4(-hw, hw, 0, 0);
+    output.pos = mul(output.pos, view);
+    output.pos = mul(output.pos, proj);
+    output.primID = primID;
+
+    outputStream.Append(output);
+    
+    output.pos = input[0].pos + float4(hw, -hw, 0, 0);
+    output.pos = mul(output.pos, view);
+    output.pos = mul(output.pos, proj);
+    output.primID = primID;
+
+    outputStream.Append(output);
+    
+    output.pos = input[0].pos + float4(hw, hw, 0, 0);
+    output.pos = mul(output.pos, view);
+    output.pos = mul(output.pos, proj);
+    output.primID = primID;
+
+    outputStream.Append(output);
  
     // 주의: GS는 Triangle Strips으로 출력합니다.
     // https://learn.microsoft.com/en-us/windows/win32/direct3d9/triangle-strips
-
+    
     // outputStream.RestartStrip(); // Strip을 다시 시작
 }
